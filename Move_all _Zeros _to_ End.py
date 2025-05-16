6c53a4511584b65f2fa1e5340e86d47ab74f789f
James Unekwuojo <unekwuojoabah4@gmail.com>
2025-05-16 22:48:31 +0100

    Update Move_all _Zeros _to_ End.py

--- Move_all _Zeros _to_ End.py
+++ Move_all _Zeros _to_ End.py
@@ -15,3 +15,26 @@ The operation must be performed in place, meaning you should not use extra space
 
 # Time complexities: O(n) --> only one Iteration needed.
 # Space complexites: O(1) --> no extra space created, 
+# Algorithm:
+# Step 1: Create a Pointer to track the position for next non-zero element
+# Step 2: Create a loop (for) to interate thru the array
+# Step 3: Check if the current elemt at i !=0, THEN swap current element with the 0 at index 'count'
+# Step  4: Move 'count' pointer to the next position.
+def pushZerosEnd(arr):
+  count = 0
+  for i in range(len(arr)):
+      if arr[i] != 0:
+        
+        arr[1], arr[count] = arr[count], arr[1]
+        count +=1
+
+if __name__ == "__main__":
+    arr = [1, 2, 0, 4, 3, 0, 5, 0]
+    pushZerosToEnd(arr)
+        
+          
+
+
+
+
+    
