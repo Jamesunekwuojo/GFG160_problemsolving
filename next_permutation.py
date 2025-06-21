73414b2c32c5557a90c6ea0c5eb75dfb3c3933ab
James Unekwuojo <unekwuojoabah4@gmail.com>
2025-06-21 10:21:38 +0100

    Update next_permutation.py

--- next_permutation.py
+++ next_permutation.py
@@ -1,4 +1,7 @@
 """
 Lexicographical: Order of arrangement of items (e,g words or number) based on aplphabetical or numerical order. Similar to how words are 
 listed in the dictionary
+
+Permutation:figuring out how many different ways you can order a specific group of things. In this case how many different ways 
+you can arrange a number
 """
